10d5c3e4fe0d949e53a0dc4c822676266f0fd3dc
dinith vanderlan <dinithvanderlan@gmail.com>
2025-03-12 18:28:49 +1100

    Update TemperatureMeasure.ino

--- TemperatureMeasure.ino
+++ TemperatureMeasure.ino
@@ -1,10 +1,10 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include "secrets.h"
-#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+#include "ThingSpeak.h" 
 #include "DHT.h"
 
-#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTPIN 2     // Digital pin 2
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
@@ -38,7 +38,7 @@ void loop() {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(SECRET_SSID);
     while(WiFi.status() != WL_CONNECTED){
-      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network
       Serial.print(".");
       delay(5000);     
     } 
@@ -47,30 +47,26 @@ void loop() {
 
   delay(2000);
 
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+  float h = dht.readHumidity();  
+  float t = dht.readTemperature(); // Read temperature as Celsius (the default)  
+  float f = dht.readTemperature(true); // Read temperature as Fahrenheit (isFahrenheit = true)
 
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  
+  if (isnan(h) || isnan(t) || isnan(f)) { // Check if any reads failed and exit early (to try again)
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+  
+  float hif = dht.computeHeatIndex(f, h); // Calculate heat index in Fahrenheit (the default)  
+  float hic = dht.computeHeatIndex(t, h, false); // Calculate heat index in Celsius (isFahreheit = false)
 
-  // set the fields with the values
-  ThingSpeak.setField(1, t);
+  
+  ThingSpeak.setField(1, t); // set the fields with the values
   ThingSpeak.setField(2, h);
   ThingSpeak.setField(3, f);
   
-  // set the status
-  ThingSpeak.setStatus(String(h));
+  Serial.println("Temperature: "+ String(t) + " | Humidity: " + String(h) + " | Index rate: " + String(h));
   
   // write to the ThingSpeak channel 
   int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
